1aaa4a81535ed42cb521ee8794aae38091209a5a
0311204 <himansh03112004@gmail.com>
2025-01-20 20:51:29 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
 
+
 function App() {
   const [theme, setTheme] = useState('light');
 
